ea88d771c440575d2967ef6f0b12cd6fd3e0a79d
Sebastian Meier <contact@sebastianmeier.eu>
2018-05-13 10:44:44 +0200

    fine tuning

--- index.js
+++ index.js
@@ -36,9 +36,8 @@ SVIFT.vis.count = (function (data, container) {
       .attr("transform", function(d){
             var bBox = this.getBBox();
             var maxSpace = Math.max(bBox.width,bBox.height);
-            var currentScale = module.d3config;
-            module.d3config.scale = (minSpace/maxSpace) * 0.95;
-            return 'translate(' + (module.vizSize.width / 2) + ', ' + ((module.vizSize.height/2) + (this.getBBox().height*module.d3config.scale * 0.34)) + ') scale('+module.d3config.scale+')';
+            module.d3config.scale = (minSpace/maxSpace*0.9);
+            return 'translate(' + (module.vizSize.width / 2) + ', ' + ((module.vizSize.height/2) + (this.getBBox().height*module.d3config.scale * 0.3)) + ') scale('+module.d3config.scale+')';
         });
 
     module.d3config.count.text(ctext);
